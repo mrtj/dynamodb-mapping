3c5686393b62c2b1c0f062231832366e49934713
Janos Tolgyesi <janos.tolgyesi@neosperience.com>
2023-07-28 17:06:03 +0200

    Add dynamodb_mapping source code

--- dynamodb_mapping/__init__.py
+++ dynamodb_mapping/__init__.py
@@ -1,5 +1,8 @@
 """Top-level package for DynamoDB Mapping."""
 
+from .dynamodb_mapping import DynamoDBMapping
+
 __author__ = """Janos Tolgyesi"""
 __email__ = 'janos.tolgyesi@gmail.com'
 __version__ = '0.1.0'
+__all__ = ['DynamoDBMapping']
