53a2f50b644df397503458f744833cca42772ae1
Janos Tolgyesi <janos.tolgyesi@neosperience.com>
2023-08-02 09:34:20 +0200

    Add py.typed

--- setup.py
+++ setup.py
@@ -39,9 +39,10 @@ setup(
     keywords="dynamodb_mapping",
     name="dynamodb_mapping",
     packages=find_packages(include=["dynamodb_mapping", "dynamodb_mapping.*"]),
+    package_data={"dynamodb_mapping": ["py.typed"]},
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/mrtj/dynamodb_mapping",
-    version="0.1.0",
+    version="0.1.1",
     zip_safe=False,
 )
